e39cdd7614f534df582dd6377569df55d10041e2
Mavaranael <s214290@students.aie.edu.au>
2024-09-04 16:58:04 +0930

    Agent Debugged

--- AIG_Maven/BasePathAgent.cpp
+++ AIG_Maven/BasePathAgent.cpp
@@ -5,105 +5,87 @@
 namespace AIForGames
 {
 	AIForGames::BasePathAgent::BasePathAgent() {
-		m_currentNode = nullptr;
-		m_targetNode = nullptr;
+		m_currentTargetNode = nullptr;
 		m_speed = 0;
 		m_position = glm::vec2(0, 0);
-		m_currentIndex = 0;
+		m_currentTargetIndex = 0;
 	}
 
 	AIForGames::BasePathAgent::BasePathAgent(Node* startNode, float speed) {
-		m_currentNode = startNode;
-		m_targetNode = nullptr;
+		m_currentTargetNode = startNode;
 		m_speed = speed;
-		m_position = m_currentNode->m_position;
-		m_currentIndex = 0;
+		m_position = m_currentTargetNode->m_position;
+		m_currentTargetIndex = 0;
 	}
 
 	void AIForGames::BasePathAgent::Update(float deltaTime) {
 		//	If path is empty, return
-		if (m_path.empty()) { /*std::cout << "Path empty..." << std::endl;*/ return; }
-
-		//	If there's no target node, update
-		if (m_targetNode == nullptr) { m_targetNode = m_path[m_currentIndex + 1]; }
+		if (m_path.empty()) { std::cout << "Path empty..." << std::endl; return; }
 
 		//	Calculate the distance to the target node
-		glm::vec2 dxdy = m_targetNode->m_position - m_position;
-		float distToNext = sqrt(dxdy.x * dxdy.x + dxdy.y * dxdy.y);
+		glm::vec2 dispToNext = m_path[m_currentTargetIndex]->m_position - m_position;	//	Vector
+		float distToNext = sqrt(dispToNext.x * dispToNext.x + dispToNext.y * dispToNext.y);	//	Magnitude
 
 		//	Find unit vector to target node
-		glm::vec2 dxdyNormal;
-		if (distToNext > 0) {	//	Normalise vector to target node
-			dxdyNormal = dxdy / distToNext;
-		}
+		glm::vec2 dispNormal = dispToNext / distToNext;	//	Vector divided by magnitude
 
 		//	Subtract speed * deltaTime from the remaining distance to the target node
-		float deltaDist = distToNext - m_speed * deltaTime;
+		float frameMovement = distToNext - m_speed * deltaTime;	//	Frame movement
+
+		//std::cout << frameMovement << std::endl;
 
 		//	Check for overshoot
-		if (deltaDist < 0)	//	We have overshot the target node
+		if (frameMovement < 0.f)	//	We have overshot the target node
 		{
-			m_currentIndex++;
-			m_currentNode = m_targetNode;
+			std::cout << "Overshot Node..." << std::endl;
+			
+			m_currentTargetIndex++;
+
+			std::cout << "Target Index: " << m_currentTargetIndex << std::endl;
+			std::cout << "Path Length: " << m_path.size() << std::endl;
 
 			//	Check if we've reached the final node
-			if (m_currentIndex >= m_path.size())	//	We've reached the final node
+			if (m_currentTargetIndex >= m_path.size())	//	We've reached the final node
 			{
-				//	Snap position to node, and clear the path
-				m_position = m_currentNode->m_position;
-				m_targetNode = nullptr;
-				m_path.clear();
-
 				std::cout << "Reached final node..." << std::endl;
+
+				//	Snap position to node, and clear the path
+				m_position = m_currentTargetNode->m_position;
+				m_path.clear();
 			}
 			else	//	We have NOT reached the final node
 			{
 				std::cout << "Reached a node..." << std::endl;
 				
-				//	Set a new target node
-				m_targetNode = m_path[m_currentIndex + 1];
-				std::cout << "Next node position: X: " << m_targetNode->m_position.x << ", Y: " << m_targetNode->m_position.y << std::endl;
+				m_currentTargetNode = m_path[m_currentTargetIndex];
 
 				//	Calculate distance to target node from new current node
-				dxdy = m_targetNode->m_position - m_currentNode->m_position;
-				std::cout << "Difference to next: " << dxdy.x << " " << dxdy.y << std::endl;
-				distToNext = sqrt(dxdy.x * dxdy.x + dxdy.y * dxdy.y);
+				dispToNext = m_currentTargetNode->m_position - m_position;
+				std::cout << "Difference to next: " << dispToNext.x << " " << dispToNext.y << std::endl;
+				distToNext = sqrt(dispToNext.x * dispToNext.x + dispToNext.y * dispToNext.y);
 				std::cout << "Distance to next: " << distToNext << std::endl;
 
 				//	Calculate unit vector to target node
-				dxdyNormal = dxdy / distToNext;
+				dispNormal = dispToNext / distToNext;
 
 				//	Move agent to new position
-				m_position = m_targetNode->m_position - distToNext * dxdyNormal;
+				m_position = m_currentTargetNode->m_position - distToNext * dispNormal;
 				std::cout << "New position: X: " << m_position.x << ", Y: " << m_position.y << std::endl;
-
-
-
-
-
-				//	Redirect remaining movement towards next node
-				//glm::vec2 newSToNode = m_currentNode->m_position - m_path[m_currentIndex + 1]->m_position;
-				//float newDToNode = sqrt(newSToNode.x * newSToNode.x + newSToNode.y * newSToNode.y);
-				//glm::vec2 newUnitVector = newSToNode / newDToNode;
-
-				//dxdy = m_currentNode->m_position - m_path[m_currentIndex + 1]->m_position;
-
-				//m_position += (0 - deltaDist) * newUnitVector;
 			}
 		}
 		else	//	We have NOT overshot the node
 		{
 			//	Move towards next node normally
 
-			m_position += m_speed * deltaTime * dxdyNormal;
+			m_position += m_speed * deltaTime * dispNormal;
 
 			//std::cout << "Travelling..." << std::endl;
 		}
 	}
 
 	void AIForGames::BasePathAgent::GoToNode(Node* node) {
-		m_path = DijkstrasSearch(m_currentNode, node);
-		m_currentIndex = 0;
+		m_path = DijkstrasSearch(m_currentTargetNode, node);
+		m_currentTargetIndex = 0;
 	}
 
 	void AIForGames::BasePathAgent::Draw() {
--- AIG_Maven/BasePathAgent.h
+++ AIG_Maven/BasePathAgent.h
@@ -11,9 +11,8 @@ namespace AIForGames
 	public:
 		glm::vec2 m_position;	//	current position in glm::vec2
 		std::vector<Node*> m_path;	//	the path; a vector of nodes
-		int m_currentIndex;	//	the index of m_currentNode
-		Node* m_currentNode;	//	the node we're currently in/came from
-		Node* m_targetNode;	//	the node we're moving towards
+		int m_currentTargetIndex;	//	the index of m_currentNode
+		Node* m_currentTargetNode;	//	the node we're currently in/came from
 		float m_speed;	//	agent speed/distance per second
 
 	public:
